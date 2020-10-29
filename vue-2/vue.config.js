41cae81f6ee4bacfc5363f1178bd6484e149fe2d
Dave Stewart <dave@davestewart.co.uk>
2020-10-29 15:31:01 +0000

    Improve setup for Vue 2 demo

--- vue-2/vue.config.js
+++ vue-2/vue.config.js
@@ -2,6 +2,10 @@
 const path = require('path')
 
 module.exports = {
+  configureWebpack (config) {
+    config.devtool = 'source-map'
+  },
+
   chainWebpack: (config) => {
     config.resolve.symlinks(false)
     config.resolve.alias.set('vue$', path.resolve(__dirname, 'node_modules/vue/'))
